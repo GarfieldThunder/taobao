3744c3e2d5a472000c12b93c6c890cc7f6be6b17
GarfieldThunder <ctrl.xiaobin@gmail.com>
2016-11-07 13:27:56 +0800

    first commit of taobaoseach and taobaocite

---
+++ taobao_search.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+ 
+This is a temporary script file.
+"""
+ 
+import requests, re, json, sqlite3
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+ 
+class Paser:
+    neededColumns = ['category', 'comment_count', 'item_loc', 'nick', 'raw_title', 'reserve_price', 'view_price', 'view_sales']   
+    
+    def __init__(self, url):
+        self.url = url
+       
+    def unicodeChange(Str):
+        Str = Str.replace('\\u003d','=')
+        Str = Str.replace('\\u0026','&')
+        return Str
+       
+    def mainPaser(self):
+        Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"}
+        Response = requests.get(self.url, headers=Headers)
+        Interial = BeautifulSoup(Response.content, 'lxml')
+        pageConfig = Interial.find('script', text=re.compile('g_page_config'))
+        return pageConfig.string
+       
+    def dataMaker(self):
+        gPageConfig = re.search(r'g_page_config = (.*?);\n', self.mainPaser())
+        pageConfigJson = json.loads(gPageConfig.group(1))
+        pageItems = pageConfigJson['mods']['itemlist']['data']['auctions']
+        pageItemsJson = json.dumps(pageItems)
+        pageData = pd.read_json(pageItemsJson)
+        neededData = pageData[Paser.neededColumns]
+        cityData = neededData['item_loc'].str.split(' ', expand = True)
+        cityData.fillna(method = 'pad', axis= 1, inplace= True)
+        neededData.loc[:,('item_loc')] = cityData[1]
+        neededData.loc[:,('view_sales')] = neededData['view_sales'].str.extract('([\d]*)([\w]*)').get(0)
+        neededData.loc[:,('view_sales')] = neededData['view_sales'].astype(int)
+        timeNow = datetime.datetime.now()
+        neededData['time'] = timeNow.strftime('%Y%m%d%H')
+        pd.to_numeric(neededData, errors = 'ignore')
+        return neededData.drop(0, axis = 0)
+       
+    def pagerMaker(self):
+        pagerRe = re.search(r'\"pager\":\"(.*?)\",\"tab\"', self.mainPaser())
+        pagerStrUni = pagerRe.group(1)
+        pagerStr = Paser.unicodeChange(pagerStrUni)
+        return 'https:'+ pagerStr + '&s='
+       
+class Adddata:
+    def __init__(self, data):
+        self.data = data
+        
+    def getCenter(city):
+        conn = sqlite3.connect('citydata.db')
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS CITYDATA(citycode text primary key , name text, latitude real, longitude real);")
+        cursor.execute("SELECT latitude, longitude FROM CITYDATA WHERE name = ?", (city,))
+        res = cursor.fetchall()
+        if not res:
+            payload = {
+                'key':'c7b75d907481d9c72eec146092028173','keywords':'',
+                'subdistrict': '0','showbiz':False,'output':'json',
+                }
+            payload['keywords'] = city
+            jsonData = requests.get('http://restapi.amap.com/v3/config/district?', params = payload)
+            jsonText = jsonData.text
+            center = re.search(r'\"([\d]*\.[\d]*)\,([\d]*\.[\d]*)\"', jsonText)
+            cityCode = re.search(r'\"citycode\"\:\"(\d*)\"', jsonText)
+            latitude = float(center.group(2))
+            longitude =  float(center.group(1))
+            cursor.execute("INSERT INTO CITYDATA VALUES(?, ?, ?, ?);", [cityCode.group(1), city, latitude, longitude])
+            cursor.close()
+            conn.commit()
+            return latitude, longitude
+        else:
+            res = list(res[0])
+            return res[0], res[1]
+    
+    def updatedData(self):
+        df = self.data
+        df['latitude'], df['longitude'] = zip(*df['item_loc'].map(Adddata.getCenter))
+        return df
+
+class Visualize:
+    def __init__(self, data, keyword):
+        self.data = data
+        self.keyword = keyword
+    
+    def validated():
+        import plotly
+        plotly.tools.set_credentials_file(username='Garfield_Liang', api_key='iqntqkqahj')
+    
+    def scatterGeo(self):
+        import plotly.plotly as py
+        
+        df = self.data
+        length = len(df)
+        limits = [(0, int(0.05*length)),(int(0.05*length), int(0.2*length)),(int(0.2*length),int(0.5*length)),(int(0.5*length),length)]
+        colors = ["#0A3854","#3779A3", "#1B85C6", "#C0DAEA"]
+        cities = []
+        
+        for i in range(len(limits)):
+            lim = limits[i]
+            df_sub = newData[lim[0]:lim[1]]
+            city = dict(
+                type = 'scattergeo',
+                locationmode = 'china',
+                lon = df_sub['longitude'],
+                lat = df_sub['latitude'],
+                text = df_sub['nick'],
+                marker = dict(
+                    size = df_sub['view_sales']/10,
+                    color = colors[i],
+                    line = dict(width = 0.5, color = '#000'),
+                    sizemode = 'area',
+                    opacity = 0.5
+                ),
+                name = "{0} - {1}".format(lim[0], lim[1])
+            )
+            cities.append(city)
+            
+        layout = dict(
+            title = self.keyword + "的淘宝分布",
+            showlegend = True,  
+            geo = dict(
+                scope = "asia",
+                projection = dict(type = 'mercator'),
+                showland = True,
+                landcolor = 'rgb(217, 217, 217)',
+                subunitwidth=1,
+                countrywidth=1,
+                resolution = 50,
+                subunitcolor="rgb(255, 255, 255)",
+                countrycolor="rgb(255, 255, 255)",
+                lonaxis = dict(range = [newData['longitude'].min()-3, newData['longitude'].max() + 3]),
+                lataxis = dict(range = [newData['latitude'].min()-0.5, newData['latitude'].max() + 0.5]),
+            ),
+        )
+        fig = dict(data = cities, layout = layout)
+        py.image.save_as(fig, 'd:/my_plot.png', scale = 10)
+    
+def showPic(pic):
+    from IPython.display import Image
+    Image(filename = pic)
+
+if __name__ == "__main__":
+   
+    Url = 'https://s.taobao.com/search?q='
+    Keyword = '小黑伞'
+    Pages = 1
+    
+    Pager = Paser(Url+Keyword).pagerMaker()
+    dataSet = pd.DataFrame()
+    for i in range(Pages):
+        newMaker = Paser(Pager + str(i))
+        newData = newMaker.dataMaker()
+        dataSet = dataSet.append(newData, ignore_index = True)
+    newData = Adddata(dataSet).updatedData()
+    newGeo = Visualize(newData, Keyword).scatterGeo()
+    showPic('d:/my_plot.png')
---
+++ taobao_site.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 16 21:10:42 2016
+
+@author: garfield
+"""
+# 2016-10-15 重新修改
+# 2016-10-17 采集框架更新
+# 2016-10-18 更新版程序完成，可以用来采集天猫商店的评论；为了可以加快运行，后续更新版本应该往迭代器方向考虑
+
+import requests
+import re, os, time, json
+import pandas as pd
+import random
+from pandas.tseries.offsets import Day
+
+class itemList:
+    
+    def __init__(self, homePage):
+        self.homePage = homePage
+    
+    def getList(self):
+#        proxy = {"http":'122.244.20.112:8998'}
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
+        urlEnd = "/search.htm?search=y"
+        content = requests.get(self.homePage+urlEnd, headers = headers)
+        valueEnd = re.findall('id=\"J_ShopAsynSearchURL\" type=\"hidden\" value=\"(.*?)\" />', content.text)
+        itemPage = self.homePage+valueEnd[0]
+        headers["referer"] = content.url
+        newContent = requests.get(itemPage, headers = headers)
+        idList = re.findall(r'data-id=\\\"(\d+)\\\">', newContent.text)
+        titleList = re.findall(r'<img alt=\\\"(.*?)\\\"', newContent.text)
+        itemList = dict(zip(idList, titleList))
+        return itemList
+
+class taobaoComment:
+    
+    def __init__(self, item, itemList, startTime, during = 7):
+        self.itemID = item
+        self.itemTitle = itemList[item]
+        self.startTime = startTime
+        self.during = during
+        
+    def getAppend(self, row):
+        appendComment = row["appendComment"]
+        if appendComment == "":
+            row["appentdDate"] = ""
+            row["appendComment"] = ""
+            return row
+        else:
+            row["appentdDate"] = appendComment["days"]
+            row["appendComment"] = appendComment["content"]
+            return row
+        
+    def getText(self, page):
+        random.seed()
+        time.sleep(1)
+        times = 0
+        url = "https://rate.tmall.com/list_detail_rate.htm"
+        params = {"itemID": self.itemID, 
+                  "sellerID": "1652864050",
+                  "order": "1",
+                  "currentPage": page,
+                  "append": "0"}
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+                   "referer": "https://detail.tmall.com/item.htm?id="+self.itemID+"&scene=taobao_shop"}
+        while True:
+            try:
+                interial = requests.get(url, params = params, headers = headers)
+            except ConnectionError:
+                print("ConnectionError, try to extend the timeout")
+                interial = requests.get(url, params = params, headers = headers, timeout = 60)
+            if "rgv587_flag" not in interial.text:
+                print("Page %d Finised.." % (page))
+                return interial.text
+            else:
+                time.sleep(abs(random.normalvariate(3, 1)))
+                headers["referer"] = interial.url
+                times += 1
+                print("itemID: " +self.itemID+" Page %d retry %d times" % (page, times))
+                if times > 5:
+                    raise ConnectionError('Anti-spyder have work 5 times...And I will force stop, please cheak the url: %s'%(interial.url))
+
+    def getPager(self, text):
+        paginator = re.findall('\"paginator\":(.*?),\"rateCount\"', text)
+        return json.loads(paginator[0])
+    
+    def strReproduct(self, stringSeries):
+        pat1 = '<b></b>'
+        pat2 = '&hellip;'
+        stringSeries = stringSeries.str.replace(pat1, '')
+        stringSeries = stringSeries.str.replace(pat2, '。')
+        return stringSeries
+    
+    def getCite(self, text):
+        resultColumns = ["auctionSku", "displayUserNick", "rateDate", "rateContent", "appendComment"]
+        jsonFile = re.findall('\"rateList\":(\[.*?\])\,\"searchinfo\"', text)
+        tempResult= pd.read_json(jsonFile[0])
+#        print(tempResult.columns)
+        if tempResult.empty:
+            print("Can't read file...")
+            return pd.DataFrame()
+        result = tempResult[resultColumns]
+        result = result.apply(self.getAppend, axis = 1)
+        result['auctionSku'] = result['auctionSku'].str.split(':').str[1]
+        result['rateDate'] = pd.to_datetime(result['rateDate'], format = "%Y-%m-%d %H:%M:%S")
+        result['rateContent'] = self.strReproduct(result['rateContent'])
+        result['appendComment'] = self.strReproduct(result['appendComment'])
+        result['itemID'] = self.itemID
+        startTime = pd.to_datetime(self.startTime, format = "%Y%m%d")
+        endTime = startTime - Day(self.during)
+        return result[result['rateDate'] > endTime]
+    
+    def mainProgram(self):
+        start = time.time()
+        print("itemID: %s( %s ), start." % (self.itemID, self.itemTitle))
+        page = 1
+        resultComment = pd.DataFrame()
+        while True:
+            pageText = self.getText(page)
+            if page == 1:
+                paginator = self.getPager(pageText)
+            tempComment = self.getCite(pageText)
+            if paginator['items'] == 0:
+                print("itemID: " +self.itemID+" is NULL!")
+                print("-----")
+                return pd.DataFrame()
+            resultComment = resultComment.append(tempComment, ignore_index = True)
+            if len(tempComment) < 20 or page == paginator['lastPage']:
+                end = time.time()
+                print("itemID: %s( %s ), Finished in %d s" % (self.itemID, self.itemTitle, (end-start)))
+                print("---")
+                return resultComment
+            else:
+                page += 1
+
+def formatOutput(writer):
+    workbook = writer.book
+    worksheet = writer.sheets['total']
+    formatOfTotal = workbook.add_format({'bold': False, 'font_color': 'black', 'align':'center', 'border':1})
+    formatOfCite = workbook.add_format({'bold': False, 'font_color': 'black', 'align':'left', 'border':1, 'text_wrap': True})
+    worksheet.set_column(0, 0, 9, formatOfTotal)
+    worksheet.set_column('B:E', 20, formatOfTotal)
+    worksheet.set_column(4, 4, 60, formatOfCite)
+    worksheet.set_column(5, 5, 30, formatOfCite)
+    worksheet.set_column(6, 6, 12, formatOfTotal)
+    worksheet.set_column(7, 7, 12, formatOfTotal)
+    
+if __name__=='__main__':
+    start = time.time()
+    print("Start at %s" % start)    
+    
+    homePage = "https://bananaumbrella.tmall.com"
+    itemList = itemList(homePage).getList()
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    writer = pd.ExcelWriter(cwd+'/cite'+time.strftime('%Y%m%d-%H%M', time.localtime(start))+'.xlsx', engine='xlsxwriter')
+    result = pd.DataFrame()
+    dayStr = input("Please enter the date,like 20161018: ")    
+    during = input("Please enter the day during: ")
+    
+    for item in itemList:
+        newMission = taobaoComment(item, itemList, dayStr, during)
+        data = newMission.mainProgram()
+        result = result.append(data, ignore_index = True)
+        result.to_excel(writer, sheet_name = "total")
+    formatOutput(writer)
+    writer.save()
+
+    end = time.time()
+    time_passed = (end - start)
+    print("time used: %d s"% (time_passed))
